05030da61ba4256e2866a9ba9ce9bba99743634a
payofix <payofix@users.noreply.github.com>
2019-01-11 17:20:13 +0200

    Update mcrypt.rb

--- opsworks_initial_setup/recipes/mcrypt.rb
+++ opsworks_initial_setup/recipes/mcrypt.rb
@@ -1,5 +1,7 @@
 # install the mcrypt
 execute "install-mcrypt" do
   command "sudo apt-get install php5-mcrypt"
+  command "sudo php5enmod mcrypt"
+  cammnd "sudo service apache2 restart" 
   action :run
 end
