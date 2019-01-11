bce8245a5350a276f8bcb50bc7cb4f311827b934
payofix <payofix@users.noreply.github.com>
2019-01-11 17:43:08 +0200

    Delete mcrypt.rb

--- opsworks_initial_setup/recipes/mcrypt.rb
+++
@@ -1,7 +0,0 @@
-# install the mcrypt
-execute "install-mcrypt" do
-  command "sudo apt-get install php5-mcrypt"
-  command "sudo php5enmod mcrypt"
-  command "sudo service apache2 restart" 
-  action :run
-end
