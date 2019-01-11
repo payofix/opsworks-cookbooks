dbaa1bba5e5cc191051fa9cedbb17969db353131
payofix <payofix@users.noreply.github.com>
2019-01-11 16:51:10 +0200

    Update metadata.rb

--- opsworks_initial_setup/metadata.rb
+++ opsworks_initial_setup/metadata.rb
@@ -5,7 +5,6 @@ license     "Apache 2.0"
 version     "1.0.0"
 
 depends "opsworks_commons"
-depends "apt"
 
 recipe "opsworks_initial_setup::sysctl", "Sets some sysctls to improve network performance"
 recipe "opsworks_initial_setup::bind_mounts", "Set up some bind mounts for apps, logs & co"
